45cbf7d48e9e43f304ef105e50650163e86254e6
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-01-15 09:36:44 -0700

    format

--- src/main/java/org/alex/games/bj/beans/CARDLETTER.java
+++ src/main/java/org/alex/games/bj/beans/CARDLETTER.java
@@ -1,5 +1,16 @@
 package org.alex.games.bj.beans;
 
 public enum CARDLETTER {
-	A,v2,v3,v4,v5,v6,v7,v8,v9,v10,J,Q,K;	
+	A(1), V2(2), V3(3), V4(4), V5(5), V6(6), V7(7), V8(8), V9(9), V10(10), J(10), Q(10), K(10);
+
+	private int value;
+
+	CARDLETTER(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
 }
--- src/main/java/org/alex/games/bj/beans/SUITS.java
+++ src/main/java/org/alex/games/bj/beans/SUITS.java
@@ -1,19 +1,17 @@
 package org.alex.games.bj.beans;
 
 public enum SUITS {
+	//they must be in this order to match the image on games/src/main/resources/deck-of-cards.jpg
 	CLUBS("♣"), DIAMONDS("♦"), HEARTS("♥"), SPADES("♠");
 
-    private String value;
+	private String value;
 
-    private SUITS(String value) {
-        this.value = value;
-    }
+	private SUITS(String value) {
+		this.value = value;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public static SUITS[] suits = new SUITS[]{
-        CLUBS, DIAMONDS, HEARTS, SPADES
-    };
 }
