16d2012db68ab30bdaba969eb5615300f61ffb03
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-01-20 10:53:25 -0700

    format

--- src/main/java/org/alex/games/bj/beans/Card.java
+++ src/main/java/org/alex/games/bj/beans/Card.java
@@ -7,23 +7,20 @@ public class Card {
 	private String shortName;
 	private String longName;
 	private int numValue = 0;
-	private boolean open= false;
+	private boolean open = false;
 
 	public Card(SUITS suit, CARDLETTER cardLetter) {
 		this.suit = suit;
 		this.cardLetter = cardLetter;
-		shortName = cardLetter.name().replaceAll("v", "");		
-		//longName = suit.name().substring(0,1) + cardLetter.name().replaceAll("v", "");
-		longName = suit.getValue() + cardLetter.name().replaceAll("v", "");
-		numValue =  cardLetter.ordinal() + 1;
-		//FIXME: This is needed for BJ
-		//But not for spider
-		//if (numValue > 10) numValue=10;
+		shortName = cardLetter.name().replace("v", "");
+		longName = suit.getValue() + shortName;
+		numValue = cardLetter.ordinal() + 1;
 	}
 
 	/**
 	 * 2,3,4,5,6,7,8,9, 10, J, Q, K, A
-	 * @return the shortName  
+	 * 
+	 * @return the shortName
 	 */
 	public String getShortName() {
 		return shortName;
@@ -31,23 +28,27 @@ public class Card {
 
 	/**
 	 * 2,3,4,5,6,7,8,9, 10 (J, Q and K return 10) , A
-	 * @return the shortName  
+	 * 
+	 * @return the shortName
 	 */
 	public String getValueShortName() {
-		if (numValue==10)
+		if (numValue == 10)
 			return "" + numValue;
 		return shortName;
 	}
 
 	/**
 	 * The Suit followed by 2,3,4,5,6,7,8,9, 10, J, Q, K, A
+	 * 
 	 * @return the longName
 	 */
 	public String getLongName() {
 		return longName;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
@@ -58,9 +59,10 @@ public class Card {
 	/**
 	 * @return true is card is Ace
 	 */
-	public boolean isA(){
-		if (cardLetter==CARDLETTER.A) return true;
-		return false;		
+	public boolean isA() {
+		if (cardLetter == CARDLETTER.A)
+			return true;
+		return false;
 	}
 
 	public int getNumValue() {
